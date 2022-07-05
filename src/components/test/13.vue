4d5b168dcac3b0dea31ca7d87b47b85de7ad5197
skw1992 <470555883@qq.com>
2022-07-05 12:21:48 +0800

    002finish

---
+++ src/components/test/13.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+const msg = "Hello World"
+
+</script>
+
+<template>
+  <!-- 将以下元素渲染成`body`的子元素 -->
+  <teleport to='body'>
+    <span>{{ msg }}</span>
+  </teleport>
+</template>
